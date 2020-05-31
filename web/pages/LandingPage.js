5acbe30f4ffac71a32a122bf2bc53507c8f01560
Jeff <jeffrey.manful@gmail.com>
2020-05-31 18:13:50 +0100

    Landing page

--- web/pages/LandingPage.js
+++ web/pages/LandingPage.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import sanityClient from '../client'
-import {getAllLinks} from '../queries'
-import PropTypes from 'prop-types'
+import {getAllCategories} from '../queries'
+// import PropTypes from 'prop-types'
 
-const LandingPage = ({links}) => {
-  console.log(links)
+const LandingPage = ({categories}) => {
+  console.log(categories)
   return (
     <Layout>
-      {links.map((l) => {
+      {categories && categories.map((cat) => {
         return (
           <div>
-            <div><a href={l.url}>{l.title}</a></div>
-            <div><a href={l.donateUrl}>Make a Donation</a></div>
-            <div>{l.category.title}</div>
+            <h2>{cat.title}</h2>
+            <div>
+              {cat.links && cat.links.map((l) => {
+                return (
+                  <div>
+                    <div><a href={l.url}>{l.title}</a></div>
+                    <div><a href={l.donateUrl}>Make a Donation</a></div>
+                  </div>
+                )
+              })}
+            </div>
           </div>
         )
       })}
+
     </Layout>
   )
 }
@@ -24,9 +33,11 @@ const LandingPage = ({links}) => {
 export default LandingPage
 
 LandingPage.propTypes = {
-  links: PropTypes.arrayOf(PropTypes.node)
+  // links: PropTypes.arrayOf(PropTypes.node),
+  categories: []
 }
 
 LandingPage.getInitialProps = async () => ({
-  links: await sanityClient.fetch(getAllLinks)
+  // links: await sanityClient.fetch(getAllLinks),
+  categories: await sanityClient.fetch(getAllCategories)
 })
