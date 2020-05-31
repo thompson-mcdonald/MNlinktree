ef992880729c575b78b5698e60a0bf023b1f6b0d
Jacob Thompson <thampsen@gmail.com>
2020-05-31 19:21:31 +0100

    bit of design

--- web/components/Layout.js
+++ web/components/Layout.js
@@ -9,34 +9,59 @@ const Nav = styled.div`
   width: 100%;
   justify-content: space-between;
   flex-direction: row;
+  align-items: center;
   height: 30px;
   margin-bottom: 20px;
+  padding: 2rem 0;
   * {
     margin-bottom: 0;
     margin-top: 0;
   }
 `
 
+const Content = styled.div`
+  width: 90%;
+  margin: 0 auto;
+  max-width: 940px;
+`
+
+const Bg = styled.div`
+  background: fff;
+  color: #111;
+  padding: 3rem 0;
+`
+
+const Alert = styled.div`
+  background: #D15757;
+  color: white;
+  padding: .2rem .8rem;
+`
+
 function Layout (props) {
   const {children} = props
   return (
-      <>
-        <Head>
-          <meta name='viewport' content='initial-scale=1.0, width=device-width, viewport-fit=cover' />
-        </Head>
-        <div id="outer-container">
+    <Bg>
+      <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width, viewport-fit=cover' />
+      </Head>
+      <Content id='outer-container'>
         <Nav>
-          <h2>Links</h2>
-          
+          <h2>MNLINKS</h2>
+          <p>BLACK LIVES MATTER</p>
         </Nav>
 
-            <main id="page-wrap">
-              <div className='container'>
-                <div className='content'>{children}</div>
-              </div>
-            </main>
+        <Alert>
+          <p>In light of the recent protests across America in response to the murder of George Floyd; there is a large need for donations to Bail Funds that allow people to get out of jail. Protest is a crucial part of society, and no one should be left destitute by exercising their rights and fighting White Supremacy.</p>
+          <p>We’ve collated links to bail funds and GoFundMe campaigns, so you can use your one Instagram link to support multiple causes</p>
+        </Alert>
+
+        <div id='page-wrap'>
+          <div className='container'>
+            <div className='content'>{children}</div>
           </div>
-      </>
+        </div>
+      </Content>
+    </Bg>
   )
 }
 
--- web/pages/LandingPage.js
+++ web/pages/LandingPage.js
@@ -1,31 +1,87 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 import sanityClient from '../client'
 import {getAllCategories} from '../queries'
 // import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const Title = styled.div`
+  font-size: 1.8rem;
+  padding-bottom: 1rem;
+  a:link, a:visited {
+    color: black;
+    text-decoration: none;
+    border-bottom: 1px solid #888;
+  }
+`
+
+const Outer = styled.div`
+  padding: 1.5rem 0;
+  display: flex;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  @media (min-width: 840px) {
+    width: 50%;
+  }
+`
+
+const Info = styled.div`
+  a {
+    padding: .5rem;
+  }
+  a:link, a:visited {
+    color: white;
+    text-decoration: none;
+    background: #D15757;
+  }
+`
+
+const FlexBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
+
+const BoxHalf = styled.div`
+  h2 {
+    font-size: 2rem;
+    font-weight: normal;
+  }
+`
 
 const LandingPage = ({categories}) => {
   console.log(categories)
   return (
     <Layout>
+      <Head>
+        <title>MNLINKS - BLACK LIVES MATTER</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width, viewport-fit=cover' />
+      </Head>
       {categories && categories.map((cat) => {
         return (
-          <div>
+          <BoxHalf>
             <h2>{cat.title}</h2>
+            <p>{cat.description}</p>
             <div>
-              {cat.links && cat.links.map((l) => {
-                return (
-                  <div>
-                    <div><a href={l.url}>{l.title}</a></div>
-                    <div><a href={l.donateUrl}>Make a Donation</a></div>
-                  </div>
-                )
-              })}
+              <FlexBox>
+                {cat.links && cat.links.map((l) => {
+                  return (
+                    <Outer>
+                      <Title><a href={l.url}>{l.title}</a></Title>
+                      {l.donateUrl && (
+                        <Info><a href={l.donateUrl}>Make a Donation</a></Info>
+                      )}
+                    </Outer>
+                  )
+                })}
+              </FlexBox>
             </div>
-          </div>
+          </BoxHalf>
         )
       })}
-
     </Layout>
   )
 }
