f5acd277d87dc86fad7c2f8ff465a6088066675b
jimmynames <jim.me.ryan@gmail.com>
2020-06-03 17:10:32 +0100

    changed src path

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -15,7 +15,6 @@ export default class MyDocument extends Document {
 
       <Html lang={this.props.lang || "en"}>
         <Head>
-          <title>SUPPORTPEOPLE.ONLINE - BLACK LIVES MATTER</title>
           <meta name="title" content="BLACK LIVES MATTER" />
           <meta name="description" content="Support those on the ground, online" />
 
@@ -23,13 +22,13 @@ export default class MyDocument extends Document {
           <meta property="og:url" content="https://supportpeople.online/" />
           <meta property="og:title" content="BLACK LIVES MATTER" />
           <meta property="og:description" content="Support those on the ground, online" />
-          <meta property="og:image" content="/public/summary_large_image.png" />
+          <meta property="og:image" content="/summary_large_image.png" />
 
           <meta property="twitter:card" content="summary_large_image.png" />
           <meta property="twitter:url" content="https://supportpeople.online/" />
           <meta property="twitter:title" content="BLACK LIVES MATTER" />
           <meta property="twitter:description" content="Support those on the ground, online" />
-          <meta property="twitter:image" content="/public/summary_large_image.png" />
+          <meta property="twitter:image" content="/summary_large_image.png" />
         </Head>
 
         <body>
